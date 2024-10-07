a077ea96a64f52feb340b945000d51f970ea0cfa
Shriyash Dongarkar <shriyashdongarkar@gmail.com>
2024-10-08 00:05:10 +0530

    Completed Assignment 7

--- Assignment_7/Problem_Statement_2/server.js
+++ Assignment_7/Problem_Statement_2/server.js
@@ -1,6 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+
+const authenticateAdmin = (id, pass) => {
+  return id == "Shriyash" && pass == "123456";
+};
+
+dotenv.config({
+  path: "./env",
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Home Page");
@@ -28,11 +37,20 @@ app.get("/products/:category/:productId", (req, res) => {
   });
 });
 
+app.get("/private/:id/:pass", (req, res) => {
+  const { id, pass } = req.params;
+  if (authenticateAdmin(id, pass)) {
+    res.send("Authentication Successful");
+  } else {
+    res.send("Authentication not Successful, Access Denied");
+  } 
+});
+
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
